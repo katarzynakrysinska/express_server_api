cc969886c50e00f885a71d464af1259b5e477309
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2022-02-21 20:52:29 +0100

    Initial order ticket

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
@@ -15,8 +20,4 @@ app.use('/api', seatsRoutes);
 
 app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
-});
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
 });
